a9b172335da53dd6c2b38c57f4cbbf66ce888a97
095Logic <adipatel095@gmail.com>
2025-03-19 11:51:43 -0400

    Renamed some frontend names for nav bar

--- client/src/components/NavigationBar.tsx
+++ client/src/components/NavigationBar.tsx
@@ -21,13 +21,13 @@ const AppNavbar = () => {
          <Navbar.Collapse id='navbar' className='d-flex flex-row-reverse'>
            <Nav className='ml-auto d-flex'>
              <Nav.Link as={Link} to='/exercises'>
-               Search For Workouts
+               Create Workout
              </Nav.Link>
              {/* if user is logged in show saved books and logout */}
              {Auth.loggedIn() ? (
                <>
                  <Nav.Link as={Link} to='/saved'>
-                   See Your Workouts
+                   My Workouts
                  </Nav.Link>
                  <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
                </>
